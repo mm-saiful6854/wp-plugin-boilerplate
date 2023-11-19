5cef170153b77b1f055879a4beb7e9e978054cf2
mm-saiful6854 <cse.saiful2119@gmail.com>
2023-11-19 09:36:58 +0600

    sub menu in wp admin dashboard

--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -4,23 +4,55 @@
  * @package  MySuperPluginName
  */
 
+require_once PLUGIN_PATH . 'inc/Api/SettingsApi.php';
 
 
 class Admin
 {
+
+	public $settings;
+
+	public $pages = array();
+	public $subpages = array();
+
+	public function __construct()
+	{
+		$this->settings = new SettingsApi();
+
+		$this->pages = array(
+			array(
+				'page_title' => 'Super Plugin Setting',
+				'menu_title' => 'SMS Gateway Settings',
+				'capability' => 'manage_options',
+				'menu_slug' => 'my_super_plugin_name',
+				'callback' => function() {require_once PLUGIN_PATH . 'templates/settings.php';},
+				'icon_url' => 'dashicons-store',
+				'position' => 110
+			)
+		);
+
+		$this->subpages = array(
+			array(
+				'parent_slug' => 'my_super_plugin_name', 
+				'page_title' => 'submenu 1', 
+				'menu_title' => 'submenu 1', 
+				'capability' => 'manage_options', 
+				'menu_slug' => 'submenu_1', 
+				'callback' => function() { echo '<h1>submenu 1</h1>'; }
+			),
+			array(
+				'parent_slug' => 'my_super_plugin_name', 
+				'page_title' => 'Custom Taxonomies', 
+				'menu_title' => 'submenu 2', 
+				'capability' => 'manage_options', 
+				'menu_slug' => 'submenu_2', 
+				'callback' => function() { echo '<h1>submenu 2</h1>'; }
+			)
+		);
+	}
+
 	public function register()
 	{
-		add_action('admin_menu', array($this, 'add_admin_pages'));
-	}
-
-	public function add_admin_pages()
-	{
-		//add_menu_page is wp built-in method to add menu page.
-		add_menu_page('Super Plugin Setting', 'Super Plugin Controller', 'manage_options', 'my_super_plugin_name', array($this, 'admin_index'), 'dashicons-store', 110);
-	}
-
-	public function admin_index()
-	{
-		require_once PLUGIN_PATH . 'templates/admin.php';
+		$this->settings->addPages( $this->pages )->withSubPage( 'Dashboard' )->addSubPages( $this->subpages )->register();
 	}
 }
